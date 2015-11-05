d3f483395f7eb888c7328e8bc9204f10b15a239e
Jared Petersen <jpetersen11@wou.edu>
2015-11-05 10:07:56 -0800

    Added angular control over data in commandcenter

--- controllers/CommandCenterController.js
+++ controllers/CommandCenterController.js
@@ -1,6 +1,36 @@
 'use strict';
 
 angular.module('tangram').controller('CommandCenterController', function($scope) {
-    // create a message to display in our view
-    $scope.breadcrumbs = 'Command Center';
+    // 3 kanban lists
+    $scope.backlog = [
+        {title: "Task Title", project: "Project A", due: "11/13/2015"},
+        {title: "Task Title", project: "Project B", due: "11/14/2015"},
+        {title: "Task Title", project: "Project C", due: "11/17/2015"},
+        {title: "Task Title", project: "Project B", due: "11/20/2015"}
+    ];
+
+    $scope.inprogress = [
+        {title: "Task Title", project: "Project A", due: "11/11/2015"}
+    ];
+
+    $scope.complete = [
+        {title: "Task Title", project: "Project A", due: "11/10/2015"},
+        {title: "Task Title", project: "Project B", due: "11/05/2015"}
+    ];
+
+    // Activity list
+    $scope.activities = [
+        {title: "Sally added a new task"},
+        {title: "Task Title marked as complete"},
+        {title: "Task Title due date changed"},
+        {title: "More . . ."},
+    ]
+
+    // Projects list
+    $scope.projects = [
+        {title: "Project A"},
+        {title: "Project B"},
+        {title: "Project C"},
+        {title: "More . . ."},
+    ];
 });
