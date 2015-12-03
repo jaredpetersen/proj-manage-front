b14c39028d5a5238c7f14e27dea81a1d01ebc19e
Jared Petersen <jpetersen11@wou.edu>
2015-12-03 10:12:54 -0800

    Added project grid to multiple projects

--- controllers/MultipleProjectController.js
+++ controllers/MultipleProjectController.js
@@ -1,6 +1,32 @@
 'use strict';
 
-angular.module('tangram').controller('MultipleProjectController', function($scope) {
+angular.module('tangram').controller('MultipleProjectController', function($scope, ApiService, AuthService) {
     // create a message to display in our view
     $scope.message = 'This is the project page. Controlled by the MultipleProjectController';
+    $scope.projects = [];
+
+    var loadData = function() {
+        // API JSONWebToken
+        var token = AuthService.getToken();
+
+        ApiService.getProjects(token)
+        .then(
+            function success(response) {
+                $scope.projects = response.data;
+
+            },
+            function error(error) {
+                console.log(error);
+            }
+        );
+    }
+
+    // Run on page load
+    console.log(AuthService.getToken() == null);
+    if (AuthService.getToken() == null) {
+        AuthService.redirect();
+    }
+    else {
+        loadData();
+    }
 });
