941cfa64e4df45d6b4a1f763dd06771b66331457
Jared Petersen <jpetersen11@wou.edu>
2015-12-09 23:26:01 -0800

    Some work on getting task data

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,7 +1,29 @@
 'use strict';
 
-angular.module('tangram').controller('TaskController', function($scope, $stateParams) {
-    // create a message to display in our view
-    $scope.message = 'This is the task page. Controlled by the TaskController';
-    $scope.id = $stateParams.id;
+angular.module('tangram').controller('TaskController', function($scope, $stateParams, ApiService, AuthService) {
+
+    // API JSONWebToken
+    var token = AuthService.getToken();
+    $scope.task = {}
+
+    var loadData = function() {
+        ApiService.getTask(token).then (
+            function success(response) {
+                $scope.task = response.data;
+            },
+            function error(response) {
+                console.log(response);
+            }
+        );
+    }
+
+    // Run on page load
+    console.log(AuthService.getToken() == null);
+    if (AuthService.getToken() == null) {
+        AuthService.redirect();
+    }
+    else {
+        loadData();
+    }
+
 });
