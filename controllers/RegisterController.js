f5656bc594e5e0022d544ff26edec6b33eee6d8e
Jared Petersen <jpetersen11@wou.edu>
2015-12-26 01:18:27 -0800

    Cleaned up registration controller

--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -2,9 +2,11 @@
 
 angular.module('tangram').controller('RegisterController', function($scope, $state, ApiService) {
 
+    // Register the user
     $scope.register = function(user) {
-        console.log('register');
+        // Make sure both passwords match
         if (user.password1 == user.password2) {
+            // Make the registration API call
             ApiService.register(user.firstName, user.lastName, user.email, user.password1)
             .then(
                 function success(response) {
@@ -16,10 +18,10 @@ angular.module('tangram').controller('RegisterController', function($scope, $sta
                 }
             );
         }
+        // Bro, your passwords don't match.
         else {
             console.log("Passwords do not match");
         }
-
     }
 
 });
